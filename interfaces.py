4c0a4687f8b4ab37c5a65420fd6e8cef87fb2368
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-08-18 21:59:16 +0000

    formatting
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver/src/nti/intid@23171 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- interfaces.py
+++ interfaces.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-
+Intid intefaces
 
 $Id$
 """
-
 from __future__ import print_function, unicode_literals, absolute_import
 __docformat__ = "restructuredtext en"
 
@@ -15,10 +14,15 @@ logger = __import__('logging').getLogger(__name__)
 # is to get usable exceptions printed from unit tests
 # See https://github.com/nose-devs/nose/issues/511
 class IntIdMissingError(KeyError):
-	"Raised by the utility when ``getId`` fails."
+	"""
+	Raised by the utility when ``getId`` fails.
+	"""
 	def __str__(self):
 		return Exception.__str__( self )
+
 class ObjectMissingError(KeyError):
-	"Raised by the utility when ``getObject`` fails."
+	"""
+	Raised by the utility when ``getObject`` fails.
+	"""
 	def __str__(self):
 		return Exception.__str__( self )
