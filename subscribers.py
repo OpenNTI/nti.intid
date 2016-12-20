a01c06f3549aa051624f86c9ac74da410fe4286a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-12-20 04:10:02 +0000

    zope.component.handle zc.intid.interfaces.ISubscriberEvent
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver/src/nti/intid@102929 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ subscribers.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import component
+
+from zope.component import handle
+
+from zc.intid.interfaces import ISubscriberEvent
+
+@component.adapter(ISubscriberEvent)
+def subscriberEventNotify(event):
+    """
+    Event subscriber to dispatch ISubscriberEvent to interested adapters.
+    """
+    handle(event.object, event)
