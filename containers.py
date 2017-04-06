8195e9ff314895a64844ec8f9e3939ee2e7250c5
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-04-06 17:31:12 +0000

    remove deprecations
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver/src/nti/intid@110306 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- containers.py
+++ containers.py
@@ -10,4 +10,4 @@ __docformat__ = "restructuredtext en"
 logger = __import__('logging').getLogger(__name__)
 
 import zope.deprecation
-zope.deprecation.moved('nti.containers.containers')
+zope.deprecation.moved('nti.containers.datastructures')
