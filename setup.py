59d2159419d6c100a14b982e0f8f5e63084e9a69
Carlos Sanchez <papachoco@gmail.com>
2017-11-28 20:50:44 -0600

    add supporting files

---
+++ setup.py
@@ -0,0 +1,83 @@
+import codecs
+from setuptools import setup
+from setuptools import find_packages
+
+entry_points = {
+    'console_scripts': [
+    ],
+}
+
+TESTS_REQUIRE = [
+    'fudge',
+    'nti.testing',
+    'zope.dottedname',
+    'zope.testrunner',
+]
+
+
+def _read(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
+
+
+setup(
+    name='nti.intid',
+    version=_read('version.txt').strip(),
+    author='Jason Madden',
+    author_email='jason@nextthought.com',
+    description="NTI intid",
+    long_description=(
+        _read('README.rst')
+        + '\n\n'
+        + _read("CHANGES.rst")
+    ),
+    license='Apache',
+    keywords='intid',
+    classifiers=[
+        'Framework :: Zope3',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    url="https://github.com/NextThought/nti.intid",
+    zip_safe=True,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    namespace_packages=['nti'],
+    tests_require=TESTS_REQUIRE,
+    install_requires=[
+        'setuptools',
+        'Acquisition',
+        'BTrees',
+        'nti.externalization',
+        'nti.ntiids',
+        'nti.wref',
+        'six',
+        'zc.intid',
+        'zope.component',
+        'zope.deferredimport',
+        'zope.deprecation',
+        'zope.event',
+        'zope.interface',
+        'zope.intid',
+        'zope.keyreference',
+        'zope.location',
+        'zope.security',
+    ],
+    extras_require={
+        'test': TESTS_REQUIRE,
+        'docs': [
+            'Sphinx',
+            'repoze.sphinx.autointerface',
+            'sphinx_rtd_theme',
+        ],
+    },
+    entry_points=entry_points,
+)
