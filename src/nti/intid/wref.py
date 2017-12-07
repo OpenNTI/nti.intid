80df776e1ef8ee0d32fa0a50620ad60e70bc9a5d
Carlos Sanchez <papachoco@gmail.com>
2017-12-06 20:46:55 -0600

    don't use pylint code

--- src/nti/intid/wref.py
+++ src/nti/intid/wref.py
@@ -14,7 +14,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
-# pylint: disable=W0212
+# pylint: disable=protected-access
 
 import warnings
 import functools
